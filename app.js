b6d87ab621dd4f5ece1967174ca451a7502ae6db
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2022-02-13 22:58:54 +0600

    this is simple pin matcher

--- app.js
+++ app.js
@@ -1,49 +1,64 @@
+// function getPin() {
+//   const pin = parseInt(Math.random() * 10000);
+//   const pinString = pin + "";
+//   if (pinString.length == 4) {
+//     return pin;
+//   } else {
+//     return getPin();
+//   }
+// }
+// function generatePin() {
+//   const pin = getPin();
+//   document.getElementById("pin-feild").value = pin;
+// }
+
 // pin generator
-function getPin() {
-  const pin = parseInt(Math.random() * 10000);
-  const pinString = pin + "";
-  if (pinString.length == 4) {
-    return pin;
-  } else {
-    return getPin();
-  }
-}
-function generatePin() {
-  const pin = getPin();
+document.getElementById("pin-btn").addEventListener("click", function () {
+  const pin = parseInt(Math.random() * 9000 + 900);
   document.getElementById("pin-feild").value = pin;
-}
-//calculator
-document.getElementById("key-pad").addEventListener("click", function (event) {
+});
+
+// calculator
+document.getElementById("number").addEventListener("click", function (event) {
   const number = event.target.innerText;
-  const calcInput = document.getElementById("typed-numbers");
+  let calcNumber = document.getElementById("number-feild");
 
   if (isNaN(number)) {
     if (number == "C") {
-      calcInput.value = "";
+      calcNumber.value = "";
     }
   } else {
-    const previousNumber = calcInput.value;
+    const previousNumber = calcNumber.value;
     const newNumber = previousNumber + number;
-    calcInput.value = newNumber;
+    calcNumber.value = newNumber;
   }
 });
-
-function verifyPin() {
-  const pin = document.getElementById("pin-feild").value;
-  const typeNumbers = document.getElementById("typed-numbers").value;
-
+// verifying
+document.getElementById("submit-btn").addEventListener("click", function () {
+  const pinValue = document.getElementById("pin-feild");
+  let pin = pinValue.value;
+  const calcValue = document.getElementById("number-feild");
+  let number = calcValue.value;
   const match = document.getElementById("match");
   const notMatch = document.getElementById("not-match");
-  if (pin == typeNumbers) {
+  if (pin == number) {
     match.style.display = "block";
     notMatch.style.display = "none";
+    pinValue.value = "";
+    calcValue.value = "";
   } else {
     notMatch.style.display = "block";
     match.style.display = "none";
+    const tryLeft = document.getElementById("try-left");
+    let tryNumber = parseInt(tryLeft.innerText) - 1;
+
+    if (tryNumber >= 0) {
+      if (tryNumber == 0) {
+        document.getElementById("faild-message").style.display = "block";
+        document.getElementById("try").style.display = "none";
+      }
+      tryLeft.innerText = tryNumber;
+    }
+    calcValue.value = "";
   }
-}
-// let numberOfTry = document.getElementById("try").innerText;
-// console.log(numberOfTry);
-// numberOf = parseInt(numberOfTry) - 1;
-// console.log(numberOf);
-// numberOfTry.innerText = numberOf;
+});
